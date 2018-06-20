0fefb1de420f73f6ee7bf7d78b0efb51936d8d98
J. Caleb Travers <jcalebtravers@icloud.com>
2018-06-19 21:27:57 -0500

    Create Header component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Header from './Header'
 
 class App extends Component {
     constructor(props) {
@@ -7,9 +8,12 @@ class App extends Component {
 
     render() {
         return(
-            <div>Hello, World!</div>
+          <div>
+          <Header />
+          <div>Hello, World!</div>
+          </div>
         )
     }
 }
 
-export default App 
+export default App
