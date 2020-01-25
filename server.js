5f0644adee676c390df2ea641ec36891054bebad
J. Caleb Travers <jcalebtravers@icloud.com>
2020-01-25 09:58:04 -0600

    Remove create-react-app conventions for testing

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 
 app.get('/', (req, res) => {
--- webpack.common.js
+++ webpack.common.js
@@ -22,7 +22,7 @@ module.exports = {
     app: './src/index.js'
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
